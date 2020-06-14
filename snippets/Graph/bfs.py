6d12f0902ecb81362e0804eaa2c5581cfdace92c
love-adelar <love.adelar@gmail.com>
2020-06-14 15:38:10 +0900

    deque 사용하지 않고 unpacking으로 stack -> queue로 수정

--- snippets/Graph/bfs.py
+++ snippets/Graph/bfs.py
@@ -5,21 +5,34 @@
 * start: 방문한 정점 `list`
 bfs 결과는 [1, 2, 3, 4, 5, 7, 8, 6]이 되어야 함
 """
-from collections import deque
-def bfs(adjacent:dict, start:int)-> list:
-    lst = []
-    candidates = deque([start])
-    was_candidate = [False] * (len(adjacent)+1)
-    was_candidate[start] = True
+#from collections import deque
+#def bfs(adjacent:dict, start:int)-> list:
+#    lst = []
+#    candidates = deque([start])
+#    was_candidate = [False] * (len(adjacent)+1)
+#    was_candidate[start] = True
+#    while candidates:
+#        curr = candidates.popleft()
+#        for vertex in adjacent[curr]:
+#            if not was_candidate[vertex]:
+#                was_candidate[vertex] = True
+#                candidates.append(vertex)
+#        lst.append(curr)
+#    return lst
+
+def bfs(adjacent, vertex):
+    candidates = [vertex]
+    visited = [False] * (len(adjacent) + 1)
+
     while candidates:
-        curr = candidates.popleft()
-        for vertex in adjacent[curr]:
-            if not was_candidate[vertex]:
-                was_candidate[vertex] = True
-                candidates.append(vertex)
-        lst.append(curr)
-    return lst
+        curr, *candidates = candidates
+        print(curr, end = ' ')
+        for node in adjacent[curr]:
+            if not visited[node]:
+                visited[node] = True
+                candidates.append(node)
+    return visited
 
 graph = {1:[2,3], 2:[3,4,5], 3:[5,7,8], 4:[5], 5:[6],       6:[], 7:[8], 8:[]}
 start = int(input('1부터 8까지의 노드 중 하나를 고르세용: '))
-print(bfs(graph, start))
+bfs(graph, start)
