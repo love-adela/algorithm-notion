1f8784a0dfb1fb38d0c4f8f629896a9dc411dd85
love-adelar <love.adelar@gmail.com>
2020-06-14 15:43:29 +0900

    Format Document

--- snippets/Graph/bfs.py
+++ snippets/Graph/bfs.py
@@ -5,8 +5,8 @@
 * start: 방문한 정점 `list`
 bfs 결과는 [1, 2, 3, 4, 5, 7, 8, 6]이 되어야 함
 """
-#from collections import deque
-#def bfs(adjacent:dict, start:int)-> list:
+# from collections import deque
+# def bfs(adjacent:dict, start:int)-> list:
 #    lst = []
 #    candidates = deque([start])
 #    was_candidate = [False] * (len(adjacent)+1)
@@ -20,19 +20,28 @@ bfs 결과는 [1, 2, 3, 4, 5, 7, 8, 6]이 되어야 함
 #        lst.append(curr)
 #    return lst
 
+
 def bfs(adjacent, vertex):
     candidates = [vertex]
     visited = [False] * (len(adjacent) + 1)
 
     while candidates:
         curr, *candidates = candidates
-        print(curr, end = ' ')
+        print(curr, end=' ')
         for node in adjacent[curr]:
             if not visited[node]:
                 visited[node] = True
                 candidates.append(node)
     return visited
 
-graph = {1:[2,3], 2:[3,4,5], 3:[5,7,8], 4:[5], 5:[6],       6:[], 7:[8], 8:[]}
+
+graph = {1: [2, 3],
+         2: [3, 4, 5],
+         3: [5, 7, 8],
+         4: [5],
+         5: [6],
+         6: [],
+         7: [8],
+         8: []}
 start = int(input('1부터 8까지의 노드 중 하나를 고르세용: '))
 bfs(graph, start)
