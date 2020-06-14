8aa5107489db277a7d76965d696813ee1479b7cd
love-adelar <love.adelar@gmail.com>
2020-06-14 15:34:37 +0900

    변수명 알아보기 쉽게 고치고 필요 없는 코드 지움

--- snippets/Graph/dfs.py
+++ snippets/Graph/dfs.py
@@ -2,33 +2,31 @@
 # [1, 3, 8, 7, 5, 6, 2, 4] 가 return 되도록 할 것
 
 # O(N)
-def dfs(adjacent, vertex):
-    candidates = [vertex]
-    visited = []
-    while candidates:
-        u = candidates.pop()
-        visited.append(u)
-        for v in adjacent[u]:
-            if v not in visited and v not in candidates:
-                candidates.append(v)
-
-    return visited
+#def dfs(adjacent, vertex):
+#    candidates = [vertex]
+#    visited = []
+#    while candidates:
+#        curr = candidates.pop()
+#        visited.append(curr)
+#        for node in adjacent[curr]:
+#            if node not in visited and node not in candidates:
+#                candidates.append(node)
+#
+#    return visited
 
 #O(1)
 def dfs(adjacent, vertex):
     candidates = [vertex]
     visited = [False]*(len(adjacent)+1)
     while candidates:
-        u = candidates.pop()
-        print(u, end=' ')
-        for v in adjacent[u]:
-            if not visited[v]:
-                visited[v] = True
-                candidates.append(v)
+        curr = candidates.pop()
+        print(curr, end=' ')
+        for node in adjacent[curr]:
+            if not visited[node]:
+                visited[node] = True
+                candidates.append(node)
     return visited
 
 graph = {1:[2,3], 2:[3,4,5], 3:[5,7,8], 4:[5], 5:[6], 6:[], 7:[8], 8:[]}
-v = int(input('1부터 8까지의 노드를 선택해서 입력하세용 : '))
-dfs(graph, v)
-print()
-print(dfs(graph, v))
+start = int(input('1부터 8까지의 노드를 선택해서 입력하세용 : '))
+dfs(graph, start)
